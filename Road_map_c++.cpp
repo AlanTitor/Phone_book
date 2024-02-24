c07ba440e8c7124010b75eb9d0286b258539b259
ALanTitor <eldar.khadgiev2013@yandex.ru>
2024-02-24 15:56:07 +0300

    Working vers

--- Road_map_c++.cpp
+++ Road_map_c++.cpp
@@ -6,97 +6,98 @@
 
 class Phone_List {
 
-	public:
+public:
 
-		std::string name = " ";
-		std::string number = "";
+	std::string name = " ";
+	std::string number = "";
+
+	std::list<std::string> list;
+
+	void add()
+	{
+		std::cout << "Введите имя: ";
+		std::cin >> this->name;
+		list.push_back(this->name);
+		std::cout << "Введите номер телефона: ";
+		std::cin >> this->number;
+		list.push_back(this->number);		
+	}
+
+	void search()
+	{
+
+	}
+
+	void remove()
+	{
+
+	}
+
+	void show()
+	{
+		if (list.empty())
+		{
+			std::cout << "Лист пуст!";
+		}
+		else {
+			for (auto iter = list.begin(); iter != list.end(); iter++)
+			{				
+				std::cout << *iter << "\t";
+				std::cout << "\n";
+			}
+		}
+		system("pause");
+	}
+
+	void menue()
+	{
+		setlocale(LC_ALL, "russian");
+		bool close = true;		
 		
-		std::list<std::string> add(std::list<std::string> &list)
+		system("cls");
+
+		do
 		{
-			std::cout << "Введите имя: ";
-			std::cin >> this->name;
-			list.push_back(this->name);
-			std::cout << "Введите номер телефона: ";
-			std::cin >> this->number;
-			list.push_back(this->number);
-			return list;
-		}
+			int choice = 0;
+			std::cout << "Выберите действие:\n";
+			std::cout << "1) Добавить контакт\n";
+			std::cout << "2) Найти контакт\n";
+			std::cout << "3) Удалить контакт\n";
+			std::cout << "4) Отобразить контакты\n";
+			std::cout << "0) Выйти\n";
+			std::cin >> choice;
 
-		void search() 
-		{
-
-		}
-
-		void remove() 
-		{
-
-		}
-
-		void show(std::list<std::string> &list)
-		{			
-			if (list.empty())
+			switch (choice)
 			{
-				std::cout << "Лист пуст!";
+			case 1:
+				add();
+				menue();
+				break;
+			case 2:
+				search();
+				menue();
+				break;
+			case 3:
+				remove();
+				menue();
+				break;
+			case 4:
+				show();
+				menue();
+				break;
+			case 0:
+				choice = false;
+				break;
+
+
+			default:
+				std::cout << "Ошибка!\n";
+				menue();
+				break;
 			}
-			else {
-				for (auto iter = list.begin(); iter != list.end(); iter++)
-				{
-					std::cout << *iter << "\t";
-				}
-			}
-		}
 
-		void menue()
-		{
-			setlocale(LC_ALL, "russian");
-			bool close = true;
-			Phone_List phone;
-			std::list<std::string> list;
-
-			//system("cls");
-
-			do
-			{
-				int choice = 0;
-				std::cout << "Выберите действие:\n";
-				std::cout << "1) Добавить контакт\n";
-				std::cout << "2) Найти контакт\n";
-				std::cout << "3) Удалить контакт\n";
-				std::cout << "4) Отобразить контакты\n";
-				std::cout << "0) Выйти\n";
-				std::cin >> choice;
-
-				switch (choice)
-				{
-				case 1:
-					list = phone.add(list);
-					menue();
-					break;
-				case 2:
-					phone.search();
-					menue();
-					break;
-				case 3:
-					phone.remove();
-					menue();
-					break;
-				case 4:
-					phone.show(list);
-					menue();
-					break;
-				case 0:
-					choice = false;
-					break;
-
-
-				default:
-					std::cout << "Ошибка!\n";
-					menue();
-					break;
-				}
-
-			} while (!close);
-		}
+		} while (!close);
+	}
 
 };
 

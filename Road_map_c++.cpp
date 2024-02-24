9a25801283847504b512848750f31235ad660d36
ALanTitor <eldar.khadgiev2013@yandex.ru>
2024-02-24 16:55:48 +0300

    Added few features

--- Road_map_c++.cpp
+++ Road_map_c++.cpp
@@ -2,6 +2,7 @@
 #include <string>
 #include <stdlib.h>
 #include <list>
+#include <algorithm>
 
 
 class Phone_List {
@@ -17,6 +18,7 @@ public:
 	{
 		std::cout << "Введите имя: ";
 		std::cin >> this->name;
+		std::transform(name.begin(), name.end(), name.begin(), tolower);
 		list.push_back(this->name);
 		std::cout << "Введите номер телефона: ";
 		std::cin >> this->number;
@@ -26,10 +28,135 @@ public:
 	void search()
 	{
 
+		int choice = 0;
+
+		std::string searching_name = "";
+		std::string searching_number = "";
+
+		std::cout << "Выберите действие:\n";
+		std::cout << "1) Поиск по имени\n";
+		std::cout << "2) Поиск по номеру\n";
+		
+		std::cin >> choice;
+		
+
+		switch (choice)
+		{
+			case 1:
+				std::cout << "Введите имя которое ищите: ";
+				std::cin >> searching_name;
+				std::transform(searching_name.begin(), searching_name.end(), searching_name.begin(), tolower);
+
+				for (auto iter = list.begin(); iter != list.end(); iter++)
+				{
+					if (searching_name == *iter)
+					{
+						std::cout << *iter;
+						std::cout << " ";
+						std::cout << *++iter;
+						break;
+					}
+					else {
+						std::cout << "Не найдено имя!";
+						break;
+					}
+				}
+				std::cout << std::endl;
+				system("pause");
+				break;
+
+			case 2:				
+				std::cout << "Введите номер который ищите: ";
+				std::cin >> searching_number;
+
+				for (auto iter = list.begin(); iter != list.end(); iter++)
+				{
+					if (searching_number == *iter)
+					{
+						std::cout << *iter;
+						std::cout << " ";
+						std::cout << *--iter;
+						break;
+					}
+					else {
+						std::cout << "Не найден номер!";
+						break;
+					}
+				}
+				std::cout << std::endl;
+				system("pause");
+				break;
+
+			default:
+				std::cout << "Ошибка!";
+				search();
+				break;
+		}
+			
 	}
 
 	void remove()
 	{
+		int choice = 0;
+
+		std::string removing_name = "";
+		std::string removing_number = "";
+
+		std::cout << "Выберите действие:\n";
+		std::cout << "1) Удалить по имени\n";
+		std::cout << "2) Удалить по номеру\n";
+
+		std::cin >> choice;
+
+		switch (choice)
+		{
+		case 1:
+			std::cout << "Введите имя которое хотите удалить: ";
+			std::cin >> removing_name;
+			std::transform(removing_name.begin(), removing_name.end(), removing_name.begin(), tolower);
+
+			for (auto iter = list.begin(); iter != list.end(); iter++)
+			{
+				if (removing_name == *iter)
+				{
+					list.erase(iter);
+
+					break;
+				}
+				else {
+					std::cout << "Не найдено имя!";
+					break;
+				}
+			}
+			std::cout << std::endl;
+			system("pause");
+			break;
+
+		case 2:
+			std::cout << "Введите номер который хотите удалить: ";
+			std::cin >> removing_number;
+
+			for (auto iter = list.begin(); iter != list.end(); iter++)
+			{
+				if (removing_number == *iter)
+				{
+					list.erase(iter, --iter);
+					break;
+				}
+				else {
+					std::cout << "Не найден номер!";
+					break;
+				}
+			}
+			std::cout << std::endl;
+			system("pause");
+			break;
+
+		default:
+			std::cout << "Ошибка!";
+			remove();
+			break;
+		}
 
 	}
 
@@ -37,7 +164,7 @@ public:
 	{
 		if (list.empty())
 		{
-			std::cout << "Лист пуст!";
+			std::cout << "Список пуст!";
 		}
 		else {
 			for (auto iter = list.begin(); iter != list.end(); iter++)
@@ -100,9 +227,6 @@ public:
 	}
 
 };
-
-
-
 
 
 int main()
